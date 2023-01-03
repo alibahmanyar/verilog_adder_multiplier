a8a2c16f1543b9a1c4d2ef41d0534617e27e70bb
Bahman <abbahmanyar@gmail.com>
2023-01-03 19:37:29 +0330

    Implement ripple adder

--- Adders/ripple_adder.v
+++ Adders/ripple_adder.v
@@ -18,14 +18,24 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+`include "FA.v"
 module ripple_adder(
-    input [15:0] a,
-    input [15:0] b,
-    output c_in,
-    output [15:0] out,
-    input c_out
+        input [15:0] a,
+        input [15:0] b,
+        output c_in,
+        output [15:0] out,
+        output c_out
     );
 
-    
+    wire [14:0] cw; // carry wires cw[i] marks c_out[i] being connected to c_in[i+1] 
 
+    FA fa0 (.a(a[0]), .b(b[0]), .c_in(0). .c_out(cw[0]), .out(out(0)));
+
+    generate
+        for (i = 1; i < 15; i = i + 1) begin
+            FA fa1 (.a(a[i]), .b(b[i]), .c_in(cw[i-1]). .c_out(cw[i]), .out(out(i)));
+        end
+    endgenerate
+
+    FA fa2 (.a(a[15]), .b(b[15]), .c_in(cw[14]). .c_out(c_out), .out(out(15)));
 endmodule
